fbd8a7ea6ff09c250a4b64a140efa8718ca009cb
only4everock <58207188+only4everock@users.noreply.github.com>
2024-03-27 14:37:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,28 +6,32 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = 80;
 
+// Middleware to log all requests
+app.use(function(req, res, next) {
+    console.log("-------------- New Request --------------");
+    console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
+    console.log("Body:"+ JSON.stringify(req.body, null, 3));
+    next();
+});
+
 app.all('/*', function (req, res) {
-   console.log("-------------- New Request --------------");
-   console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
-   console.log("Body:"+ JSON.stringify(req.body, null, 3));
+    // Check if request is coming from pricealerts.tradingview.com
+    if (req.headers.referer && req.headers.referer.startsWith("https://pricealerts.tradingview.com")) {
+        // Make a GET request to Google.com
+        axios.get('http://api.callmebot.com/start.php?source=web&user=+905302189431&text=tradingviewebak&lang=en-US')
+        .then(response => {
+            console.log("GET request to Google.com successful");
+        })
+        .catch(error => {
+            console.error("Error making GET request to Google.com:", error);
+        });
+    } else {
+        console.log("Request not from pricealerts.tradingview.com, skipping processing.");
+    }
 
-   // Check if request is coming from pricealerts.tradingview.com
-   if (req.headers.referer && req.headers.referer.startsWith("https://pricealerts.tradingview.com")) {
-       // Make a GET request to Google.com
-       axios.get('http://api.callmebot.com/start.php?source=web&user=+905302189431&text=tradingviewebak&lang=en-US')
-       .then(response => {
-           console.log("GET request to Google.com successful");
-       })
-       .catch(error => {
-           console.error("Error making GET request to Google.com:", error);
-       });
-   } else {
-       console.log("Request not from pricealerts.tradingview.com, skipping processing.");
-   }
-
-   res.json({ message: "Thank you for the message" });
+    res.json({ message: "Thank you for the message" });
 });
 
 app.listen(port, function () {
-   console.log(`Example app listening at ${port}`);
+    console.log(`Example app listening at ${port}`);
 });
