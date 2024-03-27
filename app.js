cad5b84c46759ce497170f31ab1be7827a0e08da
only4everock <58207188+only4everock@users.noreply.github.com>
2024-03-27 03:42:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 3000;
+const port = 80;
 
 app.all('/*', function (req, res) {
    console.log("-------------- New Request --------------");
    console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
    console.log("Body:"+ JSON.stringify(req.body, null, 3));
    // Make a GET request to Google.com
-   axios.get('https://www.google.com')
+   axios.get('http://api.callmebot.com/start.php?source=web&user=+905302189431&text=tradingviewebak&lang=en-US-Standard-B')
    .then(response => {
       console.log("GET request to Google.com successful");
    })
